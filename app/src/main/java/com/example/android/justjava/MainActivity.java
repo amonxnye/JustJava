cc318d0f94a867ba27e4dd4b30eec21ab63d6600
viseshrp <viseshrprasad@gmail.com>
2015-10-25 11:30:52 +0530

    Revamp UI, reorganize code and add menu button

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -55,6 +57,29 @@ public class MainActivity extends AppCompatActivity {
         checkBox1 = (CheckBox) findViewById(R.id.checkbox1);
         checkBox2 = (CheckBox) findViewById(R.id.checkbox2);
         checkBox3 = (CheckBox) findViewById(R.id.checkbox3);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -143,7 +168,8 @@ public class MainActivity extends AppCompatActivity {
         addIfChecked(checkBox1);
         addIfChecked(checkBox2);
         addIfChecked(checkBox3);
-        orderSummary += "\n" + getString(R.string.summary_total) + NumberFormat.getCurrencyInstance().format(calculatePrice(NUMBER_OF_COFFEES));
+        orderSummary += "\n" + getString(R.string.summary_total) +
+                NumberFormat.getCurrencyInstance().format(calculatePrice(NUMBER_OF_COFFEES)) + "\n";
         return orderSummary;
     }
 
